a3ea417e9f1916068fc0a835d9a4f830ec11ef68
pebi <pebi>
1998-10-29 17:12:25 +0000

    changed Name of class

--- StochasticSimulation/Listings_Java/simulation/DataAnalysis.java
+++ StochasticSimulation/Listings_Java/simulation/DataAnalysis.java
@@ -2,17 +2,18 @@ package simulation;
 
 import java.util.Random;
 
-public final class Statistics {
+
+public final class DataAnalysis {
 
   /** Calculate a integer random number between 0 and N 
-   (including 0 and N) -- use Random class */
+      (including 0 and N) -- use Random class */
   public static int nextInteger(Random rand, int N) {
     return (int)Math.round(N*rand.nextDouble());
   }
 
   /** Calculate a histogram from data sets.
-    points have to be sorted !
-    points.length must be one less than bins. */
+      points have to be sorted !
+      points.length must be one less than bins. */
   public static int[] histogram(double[] data, int number,
                                    int bins, double[] points) {
     int[] histo = new int[bins];
