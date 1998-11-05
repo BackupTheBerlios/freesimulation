9d5fa219a9d2a8b295d0fe59962a1801f918d25a
pebi <pebi>
1998-11-05 10:50:53 +0000

    added Cauchy Distribution

--- StochasticSimulation/Listings_Java/simulation/Distribution.java
+++ StochasticSimulation/Listings_Java/simulation/Distribution.java
@@ -1,3 +1,14 @@
+package simulation;
+
+import java.util.Random;
+
 public final class Distribution {
 
+    /** Generate Cauchy distributed random numbers using
+	the quotient of two Gaussian distributed random numbers. */
+    public static double nextCauchy(Random rand) {
+	double x1=rand.nextGaussian();
+ 	double x2=rand.nextGaussian();
+	return x1/x2;
+   }
 }
