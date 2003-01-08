a092f5c11947c1f42812bb9e9a6a908bca7d5d4e
pebi <pebi>
2003-01-08 19:44:12 +0000

    GNU Lizenz eingefuegt

--- StochasticSimulation/Listings_Java/simulation/Histogram.java
+++ StochasticSimulation/Listings_Java/simulation/Histogram.java
@@ -1,3 +1,26 @@
+/**
+ * 
+ *     Copyright (C) 2002  P. Biechele, F. Petruccione
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program; if not, write to the Free Software
+ *     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *
+ *
+ *     
+ */
 package simulation;
 
 /**
@@ -11,7 +34,7 @@ package simulation;
  */
 
 import java.awt.*;
-import VisualNumerics.math.Statistics;
+import simulation.Statistics;
 import ptolemy.plot.*;
 
 /** This class allows to create a histogram from a 1D double data array.
